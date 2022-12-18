363a72f896ad4d79ebd0a634e08106765fadeaab
vinaydigarse <vinaydigarse1@gmail.com>
2022-12-19 00:39:57 +0530

    project

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './comoponents/Navbar';
+import Allroutes from './comoponents/AllRoutes';
+import MainPage from './comoponents/pages/mainpage';
+
+function App() {
+  return (
+    <div className="App">
+ 
+ 
+  <Navbar/>
+  <Allroutes/>
+    </div>
+  );
+}
+
+export default App;
